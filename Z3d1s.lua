53294eac7e252b4590e33bc1eef401b2efa12b1f
Z3d1s <hoal65940@gmail.com>
2025-08-06 04:15:36 -0400

    Create Z3d1s.lua

---
+++ Z3d1s.lua
@@ -0,0 +1,197 @@
+getgenv().ConfigsKaitun = {
+	["Block Pet Gift"] = true,
+
+	Collect_Cooldown = 60, -- cooldown to collect fruit
+	JustFuckingCollectAll = false, -- Collect all (fruit not wait mutation)
+	
+	["Low Cpu"] = true,
+	["Auto Rejoin"] = true,
+	
+	["Rejoin When Update"] = false,
+	["Limit Tree"] = {
+		["Limit"] = 150,
+		["Destroy Untill"] = 150,
+
+		["Safe Tree"] = {
+			"Moon Blossom",
+			"Fossilight",
+
+		}
+	},
+
+	Seed = {
+		Buy = {
+			Mode = "Auto", -- Custom , Auto
+			Custom = {
+				"Carrot",
+			}
+		},
+		Place = {
+			Mode = "Lock", -- Select , Lock
+			Select = {
+				"Carrot"
+			},
+			Lock = {
+				"Grand Tomato",
+				"Bone Blossom",
+
+			}
+		}
+	},
+
+	["Seed Pack"] = {
+		Locked = {
+
+		}
+	},
+
+	Events = {
+		["Cook Event"] = {
+			Minimum_Money = 30_000_000, -- minimum money to start play this event
+		},
+		["Traveling Shop"] = {
+			"Bee Egg",
+		},
+		Craft = {
+			"Anti Bee Egg",
+		},
+		Shop = {
+			"Zen Egg",
+		},
+		Start_Do_Honey = 1_000_000 -- start trade fruit for honey at money
+	},
+
+	Gear = {
+		Buy = { 
+			"Grandmaster Sprinkler",
+			"Master Sprinkler",
+			"Godly Sprinkler",
+			"Advanced Sprinkler",
+			"Basic Sprinkler",
+			"Lightning Rod",
+			"Level Up Lollipop",
+			"Medium Treat",
+			"Medium Toy",
+		},
+		Lock = {
+
+		},
+	},
+
+	Eggs = {
+		Place = {
+			"Gourmet Egg",
+			"Mythical Egg",
+			"Zen Egg",
+			"Anti Bee Egg",
+			"Bug Egg",
+			"Paradise Egg",
+			"Common Summer Egg",
+		},
+		Buy = {
+			"Bee Egg",
+			"Oasis Egg",
+			"Paradise Egg",
+			"Anti Bee Egg",
+			"Night Egg",
+			"Rare Summer Egg",
+			"Bug Egg",
+			"Mythical Egg",
+			"Common Summer Egg",
+		}
+	},
+
+	Pets = {
+		["Start Delete Pet At"] = 50,
+		["Upgrade Slot"] = {
+			["Pet"] = {
+				["Starfish"] = { 7, 100, 1, true },
+
+			},
+			["Limit Upgrade"] = 8,
+			["Equip When Done"] = {
+				["Seal"] = { 5, 100, 1 },
+				["Giant Ant"] = { 3, 100, 2 },
+
+			},
+		},
+		Favorite_LockedPet = true,
+		Locked = {
+		Locked_Pet_Age = 99, -- pet that age > 60 will lock
+			"Mochi Mouse",
+			"French Fry Ferret",
+			"Spaghetti Sloth",
+			"Sushi Bear",
+			"Mizuchi",
+			"Tsuchinoko",
+			"Football",
+			"Corrupted Kitsune",
+			"Corrupted Kodama",
+			"Scarlet Macaw",
+			"Koi",
+			"Tanchozuru",
+			"Kitsune",
+			"Capybara",
+			"Disco Bee",
+			"Butterfly",
+			"Queen Bee",
+			"Dragonfly",
+			"Raccoon",
+			"Fennec Fox",
+			"Mimic Octopus",
+			"Red Fox",
+			"Blood Owl",
+			["Giant Ant"] = 4,
+			["Seal"] = 4,
+			["Starfish"] = 10,
+			["Bee"] = 1,
+		},
+		LockPet_Weight = 7, -- if Weight >= 10 they will locked,
+		Instant_Sell = {		
+			"Shiba Inu",
+			"Seagull",
+			"Crab",
+			"Tanuki",
+			"Nihonzaru",
+			"Snail",
+			"Caterpillar",
+			"Giant Ant",
+			"Bagel Bunny",
+			"Pancake Mole",
+			"Peacock",
+			"Flamingo",
+			"Brown Mouse",
+			"Kodama",
+		}
+	},
+
+	Webhook = {
+		UrlPet = "xxx",
+		UrlSeed = "xxx",
+		PcName = "xxx",
+
+		Noti = {
+			Seeds = {
+				"Sunflower",
+				"Dragon Pepper",
+				"Elephant Ears",
+			},
+			SeedPack = {
+				"Idk"
+			},
+			Pets = {
+				"Disco Bee",
+				"Butterfly",
+				"Mimic Octopus",
+				"Queen Bee",
+				"Dragonfly",
+				"Raccoon",
+				"Fennec Fox",
+			},
+			Pet_Weight_Noti = true,
+		}
+	},
+}
+License = "Cvzxhb46jbf7Zpk8cyqbt9HVibqwVPKA"
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Real-Aya/Loader/main/Init.lua'))()
